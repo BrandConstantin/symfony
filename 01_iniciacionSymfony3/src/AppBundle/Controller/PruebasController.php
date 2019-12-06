5b6c9d02f397e4578f3bdbf89dcf5ee8fec72814
BrandConstantin <constantin.brindu@gmail.com>
2019-12-07 00:02:10 +0100

    Doctrine, Forms, validate forms

--- 01_iniciacionSymfony3/src/AppBundle/Controller/PruebasController.php
+++ 01_iniciacionSymfony3/src/AppBundle/Controller/PruebasController.php
@@ -6,6 +6,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Curso;
+use AppBundle\Form\CursoType;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class PruebasController extends Controller
 {
@@ -33,13 +35,13 @@ class PruebasController extends Controller
     public function createAction(){
         //creamos el objeto curso
         $curso = new Curso();
-        $curso->setTitulo("Curso de Symfony 3");
-        $curso->setDescripcion("Curso básico de Symfony");
+        $curso->setTitulo('Curso de Symfony 3');
+        $curso->setDescripcion('Curso básico de Symfony');
         $curso->setPrecio(10);
 
         //llamamos a entity manager de doctrine
-        $em = $this->getDoctrine()->getEntityManager();
-        //$em = $this->getDoctrine()->getManager(); //para versiones superiores a 3.0.6
+        //$em = $this->getDoctrine()->getEntityManager();
+        $em = $this->getDoctrine()->getManager(); //para versiones superiores a 3.0.6
         //persistimos los datos, guarda los datos dentro de doctrine
         $em->persist($curso);
         //vuelva los datos de la url en la BBDD
@@ -53,4 +55,168 @@ class PruebasController extends Controller
 
         die();
     }
+
+    public function readAction(){
+        //sacamos entity manager
+        $em = $this->getDoctrine()->getManager();
+        //sacamos todos los cursos
+        $cursos_repository = $em->getRepository("AppBundle:Curso");
+        $cursos = $cursos_repository->findAll();
+
+        foreach($cursos as $curso){
+            echo $curso->getTitulo()."<hr/>";
+            echo $curso->getDescripcion()."<hr/>";
+            echo $curso->getPrecio()."<hr/>";
+        }
+
+        die();
+    }
+
+    public function updateAction($id, $titulo, $descripcion, $precio) {
+        //llamamos al entity manager
+        $em = $this->getDoctrine()->getManager();
+        //sacamos los cursos
+        $cursos_repo = $em->getRepository("AppBundle:Curso");
+
+        //buscar un registro por id
+        $curso = $cursos_repo->find($id);
+        $curso->setTitulo($titulo);
+        $curso->setDescripcion($descripcion);
+        $curso->setPrecio($precio);
+        var_dump($curso);
+        //persistimos los datos
+        $em->persist($curso);
+        //volcamos los datos en la bd
+        $flush = $em->flush();
+
+        if ($flush != null) {
+            echo "El curso no se ha actualizado bien";
+        } else {
+            echo "Curso actualizado correctamente";
+        }
+
+        die();
+    }
+
+    public function deleteAction($id) {
+        //llamamos al entity manager
+        $em = $this->getDoctrine()->getEntityManager();
+        //sacamos los cursos
+        $cursos_repo = $em->getRepository("AppBundle:Curso");
+        //buscar un registro por id
+        $curso = $cursos_repo->find($id);
+        $em->remove($curso);
+        //volcamos los datos en la bd
+        $flush = $em->flush();
+
+        if ($flush != null) {
+            echo "El curso no se ha borrado bien";
+        } else {
+            echo "Curso borrado correctamente";
+        }
+
+        die();
+    }
+
+    public function nativeSqlAction() {
+        //llamamos al entity manager
+        $em = $this->getDoctrine()->getEntityManager();
+        //conexión a la bd
+        $db = $em->getConnection();
+
+        $query = "SELECT * FROM cursos";
+        $stmt = $db->prepare($query);
+        $params = array();
+        $stmt->execute($params);
+
+        $cursos = $stmt->fetchAll();
+
+        //mostrar los cursos
+        foreach ($cursos as $curso) {
+            echo $curso["titulo"] . "<br/><hr/>";
+        }
+        die();
+    }
+
+    public function dqlAction() {
+        //llamamos al entity manager
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $query = $em->createQuery(""
+                        . "SELECT c FROM AppBundle:Curso c "
+                        . "WHERE c.precio > :precio "
+                        . "")->setParameter("precio", "12");
+
+        $cursos = $query->getResult();
+
+        //mostrar los cursos
+        foreach ($cursos as $curso) {
+            echo $curso->getTitulo() . "<br/><hr/>";
+        }
+        die();
+    }
+
+    public function queryBuilderAction() {
+        //llamamos al entity manager
+        $em = $this->getDoctrine()->getEntityManager();
+        //sacamos los cursos
+        $cursos_repo = $em->getRepository("AppBundle:Curso");
+
+        $query = $cursos_repo->createQueryBuilder("c")
+                ->where("c.precio > :precio")
+//                ->orderBy("precio asc")
+                ->setParameter("precio", "12")
+                ->getQuery();
+        $cursos = $query->getResult();
+
+        //mostrar los cursos
+        foreach ($cursos as $curso) {
+            echo $curso->getTitulo() . "<br/>";
+            echo $curso->getDescripcion() . "<br/>";
+            echo $curso->getPrecio() . "<br/><hr/>";
+        }
+
+        die();
+    }
+
+	public function formAction(Request $request){
+		$curso = new Curso();
+		$form = $this->createForm(CursoType::class,$curso);
+		
+		$form->handleRequest($request);
+		
+		if($form->isValid()){
+			$status = "Formulario válido";
+			$data = array(
+				"titulo" => $form->get("titulo")->getData(),
+				"descripcion" => $form->get("descripcion")->getData(),
+				"precio" => $form->get("precio")->getData()
+			);
+		}else{
+			$status = null;
+			$data = null;
+		}
+		
+		return $this->render('AppBundle:Pruebas:form.html.twig', array(
+            'form' => $form->createView(),
+			'status' => $status,
+			'data' => $data
+        ));
+	}
+
+    public function validarEmailAction($email){
+		$emailConstraint = new Assert\Email();
+		$emailConstraint->message = "Pasame un buen correo";
+		
+		$error= $this->get("validator")->validate(
+				$email,
+				$emailConstraint
+				);
+		if(count($error)==0){
+			echo "<h1>Correo valido!!</h1>";
+		}else{
+			echo $error[0]->getMessage();
+		}
+		die();
+	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # symfony
+
+##Pasos curso iniciación Symfony 3##
+- Instalar Composer y Symfony
+- Creación proyecto
+- Instalar xdebug
+- Algunos comandos básicos
+- YML
+- Rutas
+- Redirecciones
+- GET y POST
+- Crear Bundle
+- Plantillas
+- Arrays
+- Comentarios Twig
+- Funciones
+- Filtros
+- Conexión BBDD
+- Doctrine
+- Generar entidades
+- CRUD
+- SQL nativas
+- DQL
+- Query Builder
+- Formularios
+- Validar formularios
