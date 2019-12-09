5de2b849cd31b9a2dc3cd563f050abcbcaa6d913
BrandConstantin <constantin.brindu@gmail.com>
2019-12-10 00:21:11 +0100

    secundo proyecto, un blog

---
+++ 02_Blog/src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        /*$em = $this->getDoctrine()->getEntityManager();
+        $entry_repo = $em->getRepository("BlogBundle:Entry");
+        $entries = $entry_repo->findAll();
+
+        foreach($entries as $entry){
+            echo $entry->getTitle()."<br/>";
+            echo $entry->getCategory()->getName()."<br/>";
+            echo $entry->getUser()->getName()."<br/>";
+
+            $tags = $entry->getEntryTag();
+            foreach($tags as $tag){
+                echo $tag->getTag()->getName().", ";
+            }
+
+            echo "<hr/>";
+        }*/
+        
+        /*$em = $this->getDoctrine()->getEntityManager();
+        $category_repo = $em->getRepository("BlogBundle:Category");
+        $categories = $category_repo->findAll();
+
+        foreach($categories as $category){
+            echo $category->getName()."<br/>";
+
+            $entires = $category->getEntries();
+            foreach($entires as $entry){
+                echo $entry->getTitle().", ";
+            }
+
+            echo "<hr/>";
+        }*/
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $tag_repo = $em->getRepository("BlogBundle:Tag");
+        $tags = $tag_repo->findAll();
+
+        foreach($tags as $tag){
+            echo $tag->getName()."<br/>";
+
+            $entryTag = $tag->getEntryTag();
+            foreach($entryTag as $entry){
+                echo $entry->getEntry()->getTitle().", ";
+            }
+
+            echo "<hr/>";
+        }
+
+        die();
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ));
+    }
+}
---
+++ 02_Blog/src/BlogBundle/Entity/Category.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace BlogBundle\Entity;
+
+/**
+ * Category
+ */
+class Category
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name = 'NULL';
+
+    /**
+     * @var string
+     */
+    private $description = 'NULL';
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Category
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    protected $entry;
+    public function __construct(){
+        $this->entry = new \Doctrine\Common\Collections\ArrayCollections();
+    }
+
+    public function getEntries(){
+        return $this->entry;
+    }
+}
+
