933a19baf829dd2a90243aa6502e3bd76e91a863
BrandConstantin <constantin.brindu@gmail.com>
2019-12-16 17:00:28 +0100

    continación curso

--- 02_Blog/src/BlogBundle/Controller/UserController.php
+++ 02_Blog/src/BlogBundle/Controller/UserController.php
@@ -10,6 +10,12 @@ use BlogBundle\Form\UserType;
 
 class UserController extends Controller {
 
+    private $session;
+
+    public function __construct() {
+        $this->session = new Session();
+    }
+
     public function loginAction(Request $request) {
         $authenticationUtils = $this->get("security.authentication_utils");
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -29,30 +35,45 @@ class UserController extends Controller {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
-        
-        if ($form->isValid()) {
-            $user = new User();   
-      
-            $user->setName($form->get("name")->getData());
-            $user->setSurname($form->get("surname")->getData());
-            $user->setEmail($form->get("email")->getData());
-            $user->setPassword($form->get("password")->getData());
-            $user->setRole("ROLE_USER");
-            $user->setImagen(null);
 
-            $em = $this->getDoctrine()->getEntityManager();
-            $em->persist($user);
-            $flush = $em->flush();
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getEntityManager();
+                $user_repo = $em->getRepository("BlogBundle:User");
+                $user = $user_repo->findOneBy(array("email" => $form->get("email")->getData()));
 
-            if ($flush == null) {
-                $status = "El usuario se ha creado correctamente";
+                if (count($user) == 0) {
+                    $user = new User();
+
+                    $user->setName($form->get("name")->getData());
+                    $user->setSurname($form->get("surname")->getData());
+                    $user->setEmail($form->get("email")->getData());
+                    $user->setRole("ROLE_USER");
+                    $user->setImagen(null);
+
+                    $factory = $this->get("security.encoder_factory");
+                    $encoder = $factory->getEncoder($user);
+                    $password = $encoder->encodePassword($form->get("password")->getData(), $user->getSalt());
+                    $user->setPassword($password);
+
+                    $em = $this->getDoctrine()->getEntityManager();
+                    $em->persist($user);
+                    $flush = $em->flush();
+
+                    if ($flush == null) {
+                        $status = "El usuario se ha creado correctamente";
+                    } else {
+                        $status = "No te has registrado correctamente";
+                    }
+                } else {
+                    $status = "El usuario ya existe!!!";
+                }
             } else {
                 $status = "No te has registrado correctamente";
             }
-        } else {
-            $status = "No te has registrado correctamente";
-        }
 
+            $this->session->getFlashBag()->add("status", $status);
+        }
         return $this->render("BlogBundle:User:register.html.twig", array(
                     "error" => $error,
                     "last_username" => $lastUsername,
